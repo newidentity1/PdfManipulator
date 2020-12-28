7e1140a700ba8d992cb25a3d1c0c23430f02147b
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-12-27 19:49:00 -0500

    delete pdf file works

--- client/scripts/combine-pdf.js
+++ client/scripts/combine-pdf.js
@@ -3,15 +3,15 @@ var pdfList = new Array();
 function onCombinePdf() {
  var formData = new FormData();
  var file = {};
- var index = 1;
  for (const pdf of pdfList) {
      file = {
-         'index': index++,
+          'index': pdf.index,
           'name': pdf.name,
           'path': pdf.path,
      }
      formData.append("pdf[]", JSON.stringify(file));
  }
+ console.log(formData.getAll("pdf[]"));
 
  fetch("http://localhost:3000/", {
    method: "POST",
@@ -29,9 +29,17 @@ function onFileInputChange() {
     var pdfAddedList = document.getElementById('pdfs_selected').files;
     const pdfListWrapper = document.getElementById("filesList");
     for (const pdf of pdfAddedList) {
+        pdf.index = pdfList.length;
         pdfList.push(pdf);
         const pdfListElem = document.createElement("li")
+        pdfListElem.className = "list-group-items";
         pdfListElem.innerHTML = pdf.name;
+        const button = document.createElement('button');
+        button.className = "deleteFileButton";
+        button.innerHTML =
+          '<span><i class="fa fa-trash"></i></span>';
+        button.addEventListener("click", function() {onDeleteFile(pdf.index)});
+        pdfListElem.append(button);
         pdfListWrapper.append(pdfListElem);
     }
     document.getElementById("pdfs_selected").value = "";
@@ -44,4 +52,36 @@ function downloadPdf(url) {
   a.setAttribute("download", filename);
   a.click();
   delete a;
+}
+
+function onDeleteFile(fileIndex) {
+    pdfList.splice(fileIndex, 1);
+    const pdfListWrapper = document.getElementById("filesList");
+    pdfListWrapper.innerHTML = "";
+    for (const pdf of pdfList) {
+      pdfListWrapper.append(createPdfListElem(pdf, fileIndex)); 
+    }
+    document.getElementById("pdfs_selected").value = "";
+}
+
+function createPdfListElem(pdf, index=pdfList.length) {
+    if (pdf.index > index) {
+        pdf.index = pdf.index - 1;
+    }
+    const pdfListElem = document.createElement("li");
+    pdfListElem.className = "list-group-items";
+    pdfListElem.innerHTML = pdf.name;
+    const button = document.createElement("button");
+    button.className = "deleteFileButton";
+    button.innerHTML = '<span><i class="fa fa-trash"></i></span>';
+    button.addEventListener("click", function() {
+    onDeleteFile(pdf.index)});
+    pdfListElem.append(button);
+    return pdfListElem;
+}
+
+function onResetFiles() {
+    pdfList = new Array();
+    document.getElementById("filesList").innerHTML = '';
+    onFileInputChange();
 }
