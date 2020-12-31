3735d456a96abf394da102a9bccdd90b32178f5e
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-12-30 20:36:33 -0500

    added translation

--- client/scripts/combine-pdf.js
+++ client/scripts/combine-pdf.js
@@ -163,4 +163,18 @@ function disableCombineButton() {
   document.getElementById("combineButton").disabled = true;
 }
 
+function changeLanguage(languageSelect) {
+  fetch("language/" + languageSelect.value + ".json")
+  .then(response => response.json())
+  .then(responseJson => translateText(responseJson));
+}
+
+function translateText(translationJson) {
+  const elementsToTranslate = document.querySelectorAll("[data-translate]");
+  for (const element of elementsToTranslate) {
+      const key = element.getAttribute("data-translate");
+      element.textContent = translationJson[key];
+  }
+}
+
 
