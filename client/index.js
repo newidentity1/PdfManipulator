29f11adcfa2389ef73d777d17da740025eeace2b
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-12-31 02:01:24 -0500

    added color

--- client/index.js
+++ client/index.js
@@ -2,6 +2,8 @@ const {app, BrowserWindow} = require('electron');
 require('../server/app.js'); 
 
 app.on('ready', () => {
-    const window = new BrowserWindow();
+    const window = new BrowserWindow({title: "Pdf Manipulator"});
+    window.removeMenu();
+    window.maximize();
     window.loadFile('client/index.html');
 });
